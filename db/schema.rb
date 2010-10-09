142bfc2f7c031f1268a7e4b7e7deeb020a69abdf
Jason Weathered <git@jason.backtick.org>
2010-10-09 21:06:37 +1000

    Make sure email addresses are confirmed by sending an activation email.

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20101009051453) do
+ActiveRecord::Schema.define(:version => 20101009110156) do
 
   create_table "users", :force => true do |t|
     t.string   "email",                               :default => "", :null => false
@@ -27,6 +27,9 @@ ActiveRecord::Schema.define(:version => 20101009051453) do
     t.datetime "created_at"
     t.datetime "updated_at"
     t.integer  "workflowmax_staff_id"
+    t.string   "confirmation_token"
+    t.datetime "confirmed_at"
+    t.datetime "confirmation_sent_at"
   end
 
   add_index "users", ["email"], :name => "index_users_on_email", :unique => true
