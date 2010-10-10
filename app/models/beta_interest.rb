4dd2e2dc919397e3d2ccdf5ff48cb6c9b53fe483
Jason Weathered <git@jason.backtick.org>
2010-10-10 13:00:39 +1000

    Fix deprecation warnings.

--- app/models/beta_interest.rb
+++ app/models/beta_interest.rb
@@ -1,6 +1,6 @@
 class BetaInterest < ActiveRecord::Base
   validates_uniqueness_of :email
-  named_scope :unreported, :conditions => { :reported => false }
+  scope :unreported, :conditions => { :reported => false }
 
   def self.report
     unreported.each { |u| u.reported = true }
