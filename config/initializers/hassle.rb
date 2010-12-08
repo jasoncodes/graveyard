041a35c547794fea929c42353c6a8ad516265e94
Jason Weathered <jason@jasoncodes.com>
2010-12-08 21:15:19 +1000

    Move cache busting monkey patch into hassle.rb.

--- config/initializers/hassle.rb
+++ config/initializers/hassle.rb
@@ -1,1 +1,21 @@
 Rails.configuration.middleware.use Hassle
+
+# a hackity way to get cache busting for stylesheets (rails_asset_id)
+# try the original ("public/") path and if not found, try "tmp/".
+module ActionView::Helpers::AssetTagHelper
+  private
+  def rails_asset_id_with_tmp_path(source)
+    asset_id = rails_asset_id_without_tmp_path source
+    if asset_id.blank?
+      org_dir = config.assets_dir
+      begin
+        config.assets_dir = File.join(Rails.root, 'tmp', 'hassle')
+        asset_id = rails_asset_id_without_tmp_path source
+      ensure
+        config.assets_dir = org_dir
+      end
+    end
+    asset_id
+  end
+  alias_method_chain :rails_asset_id, :tmp_path
+end
